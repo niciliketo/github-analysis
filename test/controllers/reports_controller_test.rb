cdbaa42397487599038227432442674d99a8f637
Nicholas Martin <nicholas.martin@marketdojo.com>
2020-01-17 10:27:29 +0000

    Tests (#5)
    * Add repos
    
    1. Create repos model and scaffold
    2. Move the logic for generating data to repos (so it runs for every repo)
    3. Nest PRs inside Repos
    4. Correct a bad typo in my pr scaffolds
    
    * Tests
    
    1. Add some more formatting to views to make navigation easier
    2. Make tests pass

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @report = reports(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
