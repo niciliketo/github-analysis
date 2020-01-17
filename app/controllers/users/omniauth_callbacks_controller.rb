2e874d322c6958c4dcf702bd7eb1a67263303424
Nicholas Martin <nicholas.martin@marketdojo.com>
2020-01-17 10:17:46 +0000

    Users
    Create users with a way to login using github.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
+    flash[:notice] =  @user.errors.full_messages.to_sentence if !@user.valid?
     sign_in_and_redirect @user
   end
 end
