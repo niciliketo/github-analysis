a95226be83f7f16e10390e98fa07c0e49a467393
Nicholas Martin <nicholas.martin@marketdojo.com>
2020-01-17 10:26:39 +0000

    Tests
    1. Add some more formatting to views to make navigation easier
    2. Make tests pass

--- test/controllers/pull_requests_controller_test.rb
+++ test/controllers/pull_requests_controller_test.rb
@@ -1,48 +1,53 @@
 require 'test_helper'
 
+require 'test_helper'
+
 class PullRequestsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
-    @pull_reqest = pull_reqests(:one)
+    @repo = repos(:one)
+    @pull_request = pull_requests(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
-    get pull_reqests_url
+    get repo_pull_requests_url(@repo)
     assert_response :success
   end
 
   test "should get new" do
-    get new_pull_reqest_url
+    get new_repo_pull_request_url(@repo)
     assert_response :success
   end
 
-  test "should create pull_reqest" do
-    assert_difference('PullReqest.count') do
-      post pull_reqests_url, params: { pull_reqest: { creator: @pull_reqest.creator, milestone: @pull_reqest.milestone, number: @pull_reqest.number, pr_created_at: @pull_reqest.pr_created_at, pr_merged_at: @pull_reqest.pr_merged_at } }
+  test "should create pull_request" do
+    assert_difference('PullRequest.count') do
+      post repo_pull_requests_url(@repo), params: { pull_request: { creator: @pull_request.creator, milestone: @pull_request.milestone, number: @pull_request.number, pr_created_at: @pull_request.pr_created_at, pr_merged_at: @pull_request.pr_merged_at } }
     end
 
-    assert_redirected_to pull_reqest_url(PullReqest.last)
+    assert_redirected_to repo_pull_request_url(@repo, PullRequest.last)
   end
 
-  test "should show pull_reqest" do
-    get pull_reqest_url(@pull_reqest)
+  test "should show pull_request" do
+    get repo_pull_request_url(@repo, @pull_request)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_pull_reqest_url(@pull_reqest)
+    get edit_repo_pull_request_url(@repo, @pull_request)
     assert_response :success
   end
 
-  test "should update pull_reqest" do
-    patch pull_reqest_url(@pull_reqest), params: { pull_reqest: { creator: @pull_reqest.creator, milestone: @pull_reqest.milestone, number: @pull_reqest.number, pr_created_at: @pull_reqest.pr_created_at, pr_merged_at: @pull_reqest.pr_merged_at } }
-    assert_redirected_to pull_reqest_url(@pull_reqest)
+  test "should update pull_request" do
+    patch repo_pull_request_url(@repo, @pull_request), params: { pull_request: { creator: @pull_request.creator, milestone: @pull_request.milestone, number: @pull_request.number, pr_created_at: @pull_request.pr_created_at, pr_merged_at: @pull_request.pr_merged_at } }
+    assert_redirected_to repo_pull_request_url(@repo, @pull_request)
   end
 
-  test "should destroy pull_reqest" do
-    assert_difference('PullReqest.count', -1) do
-      delete pull_reqest_url(@pull_reqest)
+  test "should destroy pull_request" do
+    assert_difference('PullRequest.count', -1) do
+      delete repo_pull_request_url(@repo, @pull_request)
     end
 
-    assert_redirected_to pull_reqests_url
+    assert_redirected_to repo_pull_requests_url(@repo)
   end
 end
